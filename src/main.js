8543edab4515a53fbaf66901fb87f13945c3016a
OuaailElaouad <2003waelelaouad@gmail.com>
2024-12-01 02:22:18 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import * as THREE from 'three';
+
+const canvas = document.getElementById('canvas');
+
+//1. Scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#F0F0F0');
+
+//2.Camera
+const camera = new THREE.PerspectiveCamera(75, )
